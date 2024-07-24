96380abfdefee474614a7f6162286389367f3174
khalose <163820929+khalose@users.noreply.github.com>
2024-07-24 11:24:48 +0300

    Add files via upload

---
+++ converting to celcius 2.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int fahrenheit = 98;
+decimal temperature = ((fahrenheit - 32) * 5m / 9);
+Console.WriteLine("The temperature is " + temperature + " degrees Celsius");
+
+fahrenheit = 98;
+temperature = ((fahrenheit - 32m) * 5m / 9m);
+Console.WriteLine("The temperature is " + Math.Round(temperature) + " degrees Celsius");
+
+
